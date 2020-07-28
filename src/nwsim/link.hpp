f7ee038a7063802e0b48f702412a8ec122e65e5e
jani.arponen <jani.arponen@aalto..fi>
2020-07-28 05:13:15 +0300

    Added Node and Link, naive transmit over a Link from Node1 to Node2 working

--- src/nwsim/link.hpp
+++ src/nwsim/link.hpp
@@ -1,14 +1,38 @@
 #pragma once
 #include <queue>
+#include <memory>
 #include "packet.hpp"
 #include "node.hpp"
-
+// Need to forward declare Node in order to compile
+class Node;
 class Link {
 public:
+    Link() = delete; // don't allow default constructed Links
+    Link(std::shared_ptr<Node> n1, std::shared_ptr<Node> n2, unsigned int transmission_speed, unsigned int propagation_delay);
+    // Set new transmission speed in bytes/ms
+    void SetTransmissionSpeed(unsigned int speed) { _transmission_speed = speed; }
+    // Set new propagation delay in ms
+    void SetPropagationDelay(unsigned int delay) { _propagation_delay = delay; }
+    // "determines the interval at which new packets can be transmitted to the link" in bytes/ms
+    unsigned int GetTransmissionSpeed() const { return _transmission_speed; }
+    // "the time it takes for packet to travel across the link" in ms
+    unsigned int GetPropagationDelay() const { return _propagation_delay; }
 
+    size_t GetTransmissionQueueSize() const { return _transmission_queue.size(); }
+    Packet GetTransmitPacket();
+
+    // Naive way to transmit packets from nodes to link and link to nodes
+    void TransmitPackets();
+
+    ~Link() { }
 private:
-    // Queue currently being transmitted, holds Packet to transmit and the "address" as the Node which should receive it
-    std::queue<std::pair<Packet, Node*>> _transmission_queue;
-    unsigned int _transmission_speed; // in bits / second
-    unsigned int _propagation_delay;  // in ms
+    // Queue of currently transmitted packets.
+    // structure: To, Data
+    std::queue<std::pair<std::shared_ptr<Node>, Packet>> _transmission_queue;
+    // Pair of nodes that this link connects together
+    std::pair<std::shared_ptr<Node>, std::shared_ptr<Node>> _nodes;
+    // "determines the interval at which new packets can be transmitted to the link" in bytes/ms
+    unsigned int _transmission_speed; 
+    // "the time it takes for packet to travel across the link" in ms
+    unsigned int _propagation_delay;
 };
